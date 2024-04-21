c22aa46083c386dcd87738bc152c917096ef401b
Yousef Khaled Shawkyy <166413094+shvwkyyy@users.noreply.github.com>
2024-04-21 19:04:53 +0200

    Add files via upload

---
+++ Order.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+public class Order {
+    int customerId;
+    int orderId;
+    Product[] products;
+    double totalPrice;
+    String orderDetails;
+    public void setProducts(Product[] products) {
+        this.products = products;
+    }
+    public Order(int customerId, int orderId, double totalPrice) {
+        this.orderId = Math.abs(orderId);
+        this.customerId = Math.abs(customerId);
+        this.totalPrice = Math.abs(totalPrice);
+    }
+    public void OrderInfo() {
+        String message = "Here's your order summary:\n"+            "Order ID: " + orderId + "\n" +
+                "Customer ID: " + customerId + "\n" +            "Products:\n";
+        for (int i = 0; i < products.length; i++) {
+            message += products[i].getName() + " - $" + products[i].getPrice() + "\n";
+        }
+        message += "\nTotal Price: $" + totalPrice;
+        JOptionPane.showMessageDialog(null, message);
+        }
+    }
+
+
+
+
