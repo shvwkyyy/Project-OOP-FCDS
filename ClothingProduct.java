3f3ebb28dd477384c517fc0aaabce2922b62d363
Yousef Khaled Shawkyy <166413094+shvwkyyy@users.noreply.github.com>
2024-04-21 19:06:09 +0200

    Add files via upload

---
+++ ClothingProduct.java
@@ -0,0 +1,17 @@
+public class ClothingProduct extends Product{
+    String size,fabric;
+    public void setSize(String size) {
+        this.size = size;
+    }
+    public void setFabric(String fabric) {
+        this.fabric = fabric;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getFabric() {
+        return fabric;
+    }
+}
